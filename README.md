0ddb2f99b94713f3aced028fbbe2f01da6f4538d
Matt Haggard <haggardii@gmail.com>
2013-01-09 16:48:05 -0700

    Add doctest and travis config

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://secure.travis-ci.org/iffy/garden.png)](http://travis-ci.org/iffy/garden)
+
 Garden
 ======
 
---
+++ garden/test/test_doc.py
@@ -0,0 +1,22 @@
+from twisted.trial.unittest import TestCase
+from twisted.python.filepath import FilePath
+import doctest
+
+
+_doctest_runner = doctest.DocTestRunner(verbose=False,
+    optionflags=doctest.ELLIPSIS | doctest.IGNORE_EXCEPTION_DETAIL)
+_doctest_parser = doctest.DocTestParser()
+
+
+
+class ReadmeTest(TestCase):
+
+
+    def test_readme(self):
+        readme = FilePath(__file__).parent().parent().parent().child('README.md')
+        test = _doctest_parser.get_doctest(readme.getContent(), {},
+                                           readme.basename(), readme.path, 0)
+        output = []
+        r = _doctest_runner.run(test, out=output.append)
+        if r.failed:
+            self.fail('%s\n%s' % (test.name, ''.join(output)))
