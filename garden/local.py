b4ca04e022680013c295cb77aadeb5244357d468
Matt Haggard <haggardii@gmail.com>
2013-01-14 16:21:19 -0700

    LocalDispatcher rendered useless by interface definition change

--- garden/local.py
+++ garden/local.py
@@ -1,51 +1,5 @@
 from twisted.internet import defer
 
-from zope.interface import implements
-
-from garden.interface import (IWorkSender, IWorkReceiver, IResultSender,
-                              IResultReceiver)
-
-
-
-class LocalDispatcher(object):
-    """
-    I send work between the L{IGardener} and L{IWorker}s and send results back
-    to the L{IGardener}.
-    """
-    
-    implements(IWorkSender, IWorkReceiver, IResultSender, IResultReceiver)
-    
-    worker = None
-    gardener = None
-    
-    
-    def __init__(self, worker):
-        self.worker = worker
-        self.result_receiver = lambda *a: None
-
-
-    def sendWork(self, entity, name, version, lineage, inputs):
-        """
-        XXX
-        """
-        return self.worker.doWork(entity, name, version, lineage, inputs)
-
-
-    def sendResult(self, entity, name, version, lineage, value, inputs):
-        """
-        XXX
-        """
-        return self.gardener.workReceived(entity, name, version, lineage, value,
-                                          inputs)
-
-
-    def sendError(self, entity, name, version, lineage, error, inputs):
-        """
-        XXX
-        """
-        return self.gardener.workErrorReceived(entity, name, version, lineage,
-                                               error, inputs)
-
 
 
 class InMemoryStore(object):
