4453706fc824322e38953588eab347370fe5c7f8
Matt Haggard <haggardii@gmail.com>
2013-01-23 14:58:09 -0700

    Add tx hack

--- setup.py
+++ setup.py
@@ -17,3 +17,14 @@ setup(
         'Twisted>=12.3.0',
     ],
 )
+
+# Make Twisted regenerate the dropin.cache, if possible.  This is necessary
+# because in a site-wide install, dropin.cache cannot be rewritten by
+# normal users.
+try:
+    from twisted.plugin import IPlugin, getPlugins
+except ImportError:
+    pass
+else:
+    list(getPlugins(IPlugin))
+
